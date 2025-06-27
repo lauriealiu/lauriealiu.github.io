0977c95c46e0682f40e9e46eea4cac83f0350677
Laurie Liu <lauriealiu@gmail.com>
2025-06-27 11:39:53 -0400

    linkedin is linked in

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,12 @@ function Header() {
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="/portfolio">Resume</a></li>
-            <li><a href="#linkedIn">LinkedIn</a></li>
+            <li>
+                <a 
+                href="https://www.linkedin.com/in/lauriealiu/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >LinkedIn</a></li>
           </ul>
         </nav>
       </div>
