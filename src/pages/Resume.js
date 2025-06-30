cf131c799ee5daa3e0db44bc31399c20da8ed80d
Laurie Liu <lauriealiu@gmail.com>
2025-06-30 11:21:43 -0400

    workin' on the resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+
 function Resume() {
   return (
-    <div>
-      <h2>My Resume</h2>
-      <p>TEST TEST TEST!!!</p>
+    <div className="resume-box">
+      <h1> EXPERIENCE </h1>
+      <h2>Americorps GO Tutors July 2024 - July 2025</h2>
+      <h3> MATCH High School Math Tutor Fellow</h3>
+        <ul>
+        <li>Taught geometry to high school students, delivering engaging lessons and adapting instruction to diverse learning styles</li>
+        <li>Graded assignments and assessments promptly, providing clear and constructive feedback</li>
+        <li>Supervised students during academic and non-academic periods to maintain a safe and productive environment</li>
+        <li>Proctored exams to ensure academic integrity and support a focused testing environment</li>
+        <li>Assisted with daily logistics, including serving breakfast and supporting student transitions between activities</li>
+        <li>Fostered a positive learning environment through consistent encouragement, structure, and rapport-building</li>
+        </ul>
     </div>
   );
 }
