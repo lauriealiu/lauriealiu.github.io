68388416b4e8cbaa658b53270b479d7e4377403a
Laurie Liu <lauriealiu@gmail.com>
2025-07-01 08:54:41 -0400

    tutor photo

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import tutor from '../assets/photos/tutor.png'; 
 
 
 function Resume() {
   return (
     <div className="resume-box">
-      <h1> EXPERIENCE </h1>
-      <h2>Americorps GO Tutors July 2024 - July 2025</h2>
-      <h3> MATCH High School Math Tutor Fellow</h3>
+      <h1> WORK EXPERIENCE </h1>
+      <h2>Americorps GO Tutors</h2>
+      <h3> MATCH High School Math Tutor Fellow </h3>
+      <h4> <em>July 2024 - July 2025</em></h4>
         <ul>
-        <li>Taught geometry to high school students, delivering engaging lessons and adapting instruction to diverse learning styles</li>
-        <li>Graded assignments and assessments promptly, providing clear and constructive feedback</li>
-        <li>Supervised students during academic and non-academic periods to maintain a safe and productive environment</li>
-        <li>Proctored exams to ensure academic integrity and support a focused testing environment</li>
-        <li>Assisted with daily logistics, including serving breakfast and supporting student transitions between activities</li>
-        <li>Fostered a positive learning environment through consistent encouragement, structure, and rapport-building</li>
+        <li>Served as a full-time tutor and mentor offering daily small-group instructor to students in geometry and algebra</li>
+        <li>Supported students in classes as a computer science teaching assistant</li>
+        <li>Participated after-school programming such as staffing Girls Who Code as well as other academic support staff duties</li>
+        <li>Worked with students and teachers to set individualized and student-driven academic goals and foster student social-emotional growth </li>
+        <li>Built community relationships through parent communication and annual school-wide events</li>
         </ul>
+      <img src={tutor} alt="Me tutoring!" />
     </div>
   );
 }
