f973dc66399d77a41369791f0ac0e3eb13ea02ad
Laurie Liu <lauriealiu@gmail.com>
2025-07-01 11:35:48 -0400

    mii animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import miiImage from '../assets/photos/mii.png'; 
+import miiGIF from '../assets/photos/miiGIF.gif'; 
+import miiBlink from '../assets/photos/miiBlink.png'; 
+import React, { useState } from 'react';
 
 function Home() {
+  const [isBouncing, setIsBouncing] = useState(false);
+  const [isBlink, setBlink] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsBouncing(true);
+    setTimeout(() => setIsBouncing(false), 300); // Reset after animation
+  };
+
+  const handleClick = () => {
+    setBlink(prev => !prev);
+    setBounceClass('big-bounce');
+    setTimeout(() => setBounceClass(''), 500);
+  };
   return (
-    <div className="container">
+    <div className="container" onMouseMove={handleMouseEnter}>
       <h2 className="bigText">Hi there! I'm Laurie, an aspiring... something!</h2>
-      <img src={miiImage} alt="It's Mii!" />
+      <img 
+      src={isBlink ? miiBlink :miiGIF} 
+      alt="It's Mii!" 
+      className={isBouncing ? 'bounce' : ''} 
+      onMouseEnter={handleMouseEnter}
+      onClick={handleClick}
+      style={{ cursor: 'pointer' }}
+      />
     </div>
   );
 }
