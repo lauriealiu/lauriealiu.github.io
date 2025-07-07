52d5a68463d78f97dd9842bdbc09b8cbe62c4271
Laurie Liu <lauriealiu@gmail.com>
2025-07-07 11:31:38 -0400

    about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import me from '../assets/photos/me.png';
 
 function About() {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>TEST TEST TEST!!!</p>
+    <div className="about-container">
+      <h1>About Me</h1>
+      <h2>Hello! I'm Laurie, a passionate technologist with a background in computer science and a strong commitment to using technology as a force for good.</h2>
+      <h2>I recently earned my Bachelor's in Computer Science from UC Santa Cruz, where I also served as a peer navigator, course instructor, and International Living Center intern — experiences that deepened my passion for mentorship, leadership, and global engagement.</h2>
+      <h2>Along the way, I’ve interned as a frontend developer at Spacelab and as a computing / cybersecurity fellow at Lawrence Livermore National Laboratory, where I applied my technical skills to projects ranging from exoplanet data visualization to water system simulations.</h2>
+      <h2>Most recently, I served with AmeriCorps at MATCH Charter High School, where I tutored and mentored students in math and computer science, which was an experience that strengthened my leadership skills and deepened my commitment to educational equity.</h2>
+      <h2>In Fall 2025, I’ll begin my M.S. in Information Systems at San Diego State University, where I hope to grow as both a developer and a leader. My long-term goal is to lead diverse, mission-driven teams that build accessible and impactful tech solutions.</h2>
+{/* <img src={me} alt="It is me" style={{ maxWidth: '300px', borderRadius: '12px' }}/> */}
+
     </div>
   );
 }
