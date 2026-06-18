3c1105b242f50098dd633e8119f1258a8fb90a84
Laurie Liu <lauriealiu@gmail.com>
2026-06-18 12:31:39 -0700

    update pips & mobile

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -46,7 +46,7 @@ function Games() {
     },
     {
       name: 'Pips',
-      record: 'Easy: 0:20, Medium: 1:05, Hard: 3:04',
+      record: 'Easy: 0:13, Medium: 0:35, Hard: 3:04',
       link: 'https://www.nytimes.com/games/pips',
       note: 'Place every domino in the right spot.'
     }
