60e9a44d3e406950f3e52bf661528ade570330d2
Laurie Liu <lauriealiu@gmail.com>
2026-06-18 11:59:24 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,20 +9,28 @@ import Resume from './pages/Resume';
 import Games from './pages/Games';
 import CursorTrail from './components/CursorTrail';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <main className="app-main" key={location.pathname}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/games" element={<Games />} />
+      </Routes>
+    </main>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="app-container">
-      <CursorTrail />
+        <CursorTrail />
         <Header />
-        <main className="app-main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/games" element={<Games />} />
-          </Routes>
-        </main>
+        <AnimatedRoutes />
         <Footer />
       </div>
     </Router>
